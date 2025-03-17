b8afaf0d65e392c003e857f0fd1d0378b84b37fc
chrell99 <129071779+chrell99@users.noreply.github.com>
2025-03-17 23:21:16 +0100

    - Made the test minimal example a strobe

--- tests/minimal-example.cc
+++ tests/minimal-example.cc
@@ -25,19 +25,14 @@ static void DrawOnCanvas(Canvas *canvas) {
    * Let's create a simple animation. We use the canvas to draw
    * pixels. We wait between each step to have a slower animation.
    */
-  canvas->Fill(0, 0, 255);
-
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
+  canvas->Fill(255, 255, 255);
+  for (int i = 0; i < 20; i++) {
     if (interrupt_received)
       return;
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y, 255, 0, 0);
-    usleep(1 * 1000); // wait a little to slow down things.
+    canvas->Fill(255, 255, 255);
+    usleep(1 * 200000); 
+    canvas->Fill(0, 0, 0);
+    usleep(1 * 200000);
   }
 }
 
