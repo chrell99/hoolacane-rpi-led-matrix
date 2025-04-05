419644aa966004e589e4a2d91515caf66922965a
chrell99 <129071779+chrell99@users.noreply.github.com>
2025-04-05 23:52:47 +0200

    - Added sound handling to the strobe test

--- tests/strobe-test.cc
+++ tests/strobe-test.cc
@@ -12,21 +12,65 @@
 #include <stdio.h>
 #include <signal.h>
 
-using rgb_matrix::RGBMatrix;
+#define PCM_DEVICE "hw:1,0" // USB Dongle audio input
+#define SAMPLE_RATE 44100   // 44.1 kHz sample rate
+#define BUFFER_SIZE 1024    // FFT buffer size (must be power of 2)
+
 using rgb_matrix::Canvas;
+using rgb_matrix::RGBMatrix;
 
-
-uint64_t micros() {
+uint64_t micros()
+{
     static auto start_time = std::chrono::steady_clock::now();
     auto now = std::chrono::steady_clock::now();
 
     return std::chrono::duration_cast<std::chrono::microseconds>(now - start_time).count();
 }
 
-int main(int argc, char *argv[]) {
-    std::cout << "TIMESTAMP 1: " << micros() << std::endl;
+int configure_pcm_device(snd_pcm_t *&pcm_handle, snd_pcm_hw_params_t *&params,
+                         snd_pcm_format_t format, unsigned int rate, int channels, int buffer_size)
+{
+    if (snd_pcm_open(&pcm_handle, PCM_DEVICE, SND_PCM_STREAM_CAPTURE, 0) < 0)
+    {
+        std::cerr << "Failed to open PCM device" << std::endl;
+        return -1;
+    }
+
+    snd_pcm_hw_params_alloca(&params);
+    if (snd_pcm_hw_params_any(pcm_handle, params) < 0 ||
+        snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED) < 0 ||
+        snd_pcm_hw_params_set_format(pcm_handle, params, format) < 0 ||
+        snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0 ||
+        snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0 ||
+        snd_pcm_hw_params(pcm_handle, params) < 0)
+    {
+        std::cerr << "Failed to configure PCM device" << std::endl;
+        return -1;
+    }
+
+    if (snd_pcm_prepare(pcm_handle) < 0)
+    {
+        std::cerr << "Failed to prepare PCM device" << std::endl;
+        return -1; 
+    }
+
+    std::out << "Succesfully opened the PCM device" << std:endl;
+    return 0; 
+}
+
+int main(int argc, char *argv[]){
+
+    //************ SOUND VARS **************/
+    snd_pcm_t *pcm_handle;
+    snd_pcm_hw_params_t *params;
+    snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
+    unsigned int rate = SAMPLE_RATE;
+    int channels = 1;
+    int buffer_size = BUFFER_SIZE;
+
+    //************ RGB MATRIX VARS **************/
     RGBMatrix::Options options;
-    options.hardware_mapping = "regular"; 
+    options.hardware_mapping = "regular";
     options.rows = 32;
     options.chain_length = 3;
     options.parallel = 3;
@@ -34,18 +78,23 @@ int main(int argc, char *argv[]) {
     options.multiplexing = 1;
     rgb_matrix::RuntimeOptions rOptions;
     rOptions.gpio_slowdown = 2;
-    
-    RGBMatrix* canvas = RGBMatrix::CreateFromOptions(options, rOptions);
+
+    if (configure_pcm_device(pcm_handle, params, format, rate, channels, buffer_size) < 0) {
+        return -1; 
+    }
+
+    RGBMatrix *canvas = RGBMatrix::CreateFromOptions(options, rOptions);
     canvas->SetBrightness(50);
 
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i)
+    {
         std::cout << "Argument " << i << ": " << argv[i] << std::endl;
     }
-  
+
     if (canvas == NULL)
-      return 1;
-  
+        return 1;
+
     canvas->Clear();
     delete canvas;
     return 0;
-  }
+}
