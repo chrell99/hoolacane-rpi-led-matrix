b31121779c509053eddde24ae92817336c1d2062
chrell99 <129071779+chrell99@users.noreply.github.com>
2025-04-13 04:05:32 +0200

    - Corrected compiling errors

--- music-synced/spectrum-visualizer.cc
+++ music-synced/spectrum-visualizer.cc
@@ -177,12 +177,12 @@ int main(int argc, char *argv[]){
         snd_pcm_readi(pcm_handle, buffer.data(), buffer_size);
         magnitudes = computeFFT(buffer);
         int fftSize = magnitudes.size();
-        int temp = 1;
+        double temp = 1;
         int lastBinEnd = 0;
         for (int i = 0; i < 24; i++){
 
             int binStart = lastBinEnd;
-            int binEnd   = lastBinEnd + temp * 2;
+            int binEnd   = lastBinEnd + (int)(temp * 1.8);
             lastBinEnd = binEnd;
             temp += 2;
 
